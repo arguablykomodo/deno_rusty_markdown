737834448ae3e4528f6a0237b06ad36f403a28a6
Sr Komodo <komodosr@gmail.com>
2020-08-26 18:16:33 -0300

    docs: write some documentation

---
+++ README.md
@@ -0,0 +1,6 @@
+# `deno-rusty-markdown`
+
+Deno bindings for [`pulldown-cmark`][1], a CommonMark-compliant Markdown parser
+made in Rust, compiled to WebAssembly.
+
+[1]:https://github.com/raphlinus/pulldown-cmark
--- mod.ts
+++ mod.ts
@@ -1,14 +1,32 @@
 // @deno-types="./pkg/deno_rusty_markdown.d.ts"
 import { parse as internal_parse } from "./pkg/deno_rusty_markdown.js";
 
-interface Options {
+/**
+ * Option object containing flags for enabling extra features that are not part
+ * of the CommonMark spec.
+ */
+export interface Options {
+  /** Enables Github flavored tables. */
   tables?: boolean;
+  /**
+   * Enables footnotes[^1].
+   *
+   * [^1]: Like this.
+   */
   footnotes?: boolean;
+  /** Enables strikethrough text, ~~like this~~. */
   strikethrough?: boolean;
+  /** Enabled Github flavored task lists. */
   tasklists?: boolean;
 }
 
-function parse(text: string, options: Options = {}): string {
+/**
+ * Parses the given Markdown into HTML.
+ * @param {string} text - Source Markdown text
+ * @param {Options} options - Extra enabled features
+ * @returns {string} Parsed HTML
+ */
+export function parse(text: string, options: Options = {}): string {
   return internal_parse(
     text,
     (+(options.tables ?? false) << 1) +
@@ -17,5 +35,3 @@ function parse(text: string, options: Options = {}): string {
       (+(options.tasklists ?? false) << 4),
   );
 }
-
-export { Options, parse };

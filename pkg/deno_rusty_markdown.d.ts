e93b00420fb4f94147e85e31342d8c2b4b9786b2
Sr Komodo <komodosr@gmail.com>
2020-08-27 19:30:25 -0300

    change: commit build artifacts

---
+++ pkg/deno_rusty_markdown.d.ts
@@ -0,0 +1,8 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} text
+* @param {number} options
+* @returns {string}
+*/
+export function parse(text: string, options: number): string;

6cddd4327c324145346c4882701ba7697a6830b2
Sr Komodo <komodosr@gmail.com>
2020-08-29 01:13:14 -0300

    ci: way simpler wasm check

--- scripts/compare_checksums.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-git clean -xdf
-OLD="$(find pkg -type f -print0 | sort -z | xargs -r0 md5sum | md5sum)"
-echo "$OLD"
-deno run --allow-read --allow-write --allow-run scripts/build.ts
-git clean -xdf
-NEW="$(find pkg -type f -print0 | sort -z | xargs -r0 md5sum | md5sum)"
-echo "$NEW"
-if [ "$OLD" != "$NEW" ]; then exit 1; fi

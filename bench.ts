77e683648d817e8ba61822762c2ebd1a3103a99e
Sr Komodo <komodosr@gmail.com>
2020-08-29 15:06:56 -0300

    ci: add benchmarks

---
+++ bench.ts
@@ -0,0 +1,46 @@
+import { parse } from "./mod.ts";
+import {
+  bench,
+  runBenchmarks,
+} from "https://deno.land/std@0.67.0/testing/bench.ts";
+
+const input = "Hello world, this is a ~~complicated~~ *very simple* example.";
+
+bench({
+  name: "simple parse",
+  runs: 1000,
+  func(b): void {
+    b.start();
+    for (let i = 0; i < 1000; i++) {
+      parse(input);
+    }
+    b.stop();
+  },
+});
+
+bench({
+  name: "parse with options",
+  runs: 1000,
+  func(b): void {
+    b.start();
+    for (let i = 0; i < 1000; i++) {
+      parse(input, { strikethrough: true });
+    }
+    b.stop();
+  },
+});
+
+const inputFrontmatter = "---\nfoo: bar\n---\n" + input;
+bench({
+  name: "parse with frontmatter",
+  runs: 1000,
+  func(b): void {
+    b.start();
+    for (let i = 0; i < 1000; i++) {
+      parse(inputFrontmatter, { frontmatter: true });
+    }
+    b.stop();
+  },
+});
+
+runBenchmarks();

ca98e6ae81f57846250482108258e05fe7f0df91
Komodo <noreply@komodo.ar>
2021-08-08 23:46:31 -0300

    chore: update wasm and glue

--- wasm/deno_rusty_markdown.d.ts
+++ wasm/deno_rusty_markdown.d.ts
@@ -3,12 +3,11 @@
 /**
 * @param {string} text
 * @param {number} options
-* @returns {string}
-*/
-export function html(text: string, options: number): string;
-/**
-* @param {string} text
-* @param {number} options
 * @returns {Array<any>}
 */
-export function parse(text: string, options: number): Array<any>;
+export function tokens(text: string, options: number): Array<any>;
+/**
+* @param {Array<any>} tokens
+* @returns {string}
+*/
+export function html(tokens: Array<any>): string;
